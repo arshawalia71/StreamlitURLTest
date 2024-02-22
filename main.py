fbf4d0ccb91de9f0cbd4b9f9cfe3f60345bc7261
arshawalia71 <60817328+arshawalia71@users.noreply.github.com>
2024-02-22 22:14:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import streamlit as st
 def main():
     st.title("Main Page")
     if st.button("Go to Hello Page"):
-        # Set the URL query parameter to navigate to hello.py
-        st.experimental_set_query_params(page="hello")
+        # Set the session state variable to navigate to hello.py
+        st.session_state.page = "hello"
 
 if __name__ == "__main__":
     main()
